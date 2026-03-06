d95e94ce3c9f5c9e6229199edf4a7fbe2f1062c4
lasu <806955497@qq.com>
2026-03-06 23:32:41 +0800

    恢复通用人员选择框测试路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,7 +19,12 @@ const routes = [
         component: () => import('../views/Dashboard.vue'),
         meta: { title: '首页' }
       },
-
+      {
+        path: 'test-person-selector',
+        name: 'TestPersonSelector',
+        component: () => import('../views/TestPersonSelector.vue'),
+        meta: { title: '人员选择框测试' }
+      },
       {
         path: 'system',
         name: 'System',
