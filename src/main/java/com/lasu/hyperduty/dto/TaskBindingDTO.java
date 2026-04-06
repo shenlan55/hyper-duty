ac694b6761146af0a45688d935f72284e7e03e7f
lasu <806955497@qq.com>
2026-04-06 12:59:18 +0800

    ### 1. 排班模式管理页面 (frontend/src/views/duty/ScheduleMode.vue) - 导入了 formatDateTime 函数 - 在列配置中为 createTime 列添加了 slotName - 添加了时间格式化的插槽模板 - 同时修复了导出功能中的时间格式化 ### 2. 自定义表格页面 (frontend/src/views/project/CustomTable.vue) - 导入了 formatDateTime 函数 - 在列配置中为 createTime 列添加了 slot - 添加了时间格式化的插槽模板

---
+++ src/main/java/com/lasu/hyperduty/dto/TaskBindingDTO.java
@@ -0,0 +1,16 @@
+package com.lasu.hyperduty.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TaskBindingDTO {
+    private Long id;
+    private Long taskId;
+    private Long tableId;
+    private Long rowId;
+    private String tableName;
+    private String rowData;
+    private LocalDateTime createTime;
+}
